628c7c5a5d7a499b2abb972fd2b121d2a3125482
jvo-james <jadelstech@gmail.com>
2026-09-19 13:49:23 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // Firebase web config. Replace these values before publishing.
 window.JVO_FIREBASE_CONFIG = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBoNqDbTUsQizv2h16NXGaBYRJml3Gh85c",
+    authDomain: "jvojames-30a71.firebaseapp.com",
+    projectId: "jvojames-30a71",
+    storageBucket: "jvojames-30a71.firebasestorage.app",
+    messagingSenderId: "799357877051",
+    appId: "1:799357877051:web:c5aaddbde53901f06c151e"
 };
